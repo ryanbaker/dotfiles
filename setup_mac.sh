c29fd62afecd9843ba56afc57211d51f05476e71
Ryan Baker <ryanb@cetoncorp.com>
2013-11-11 11:28:59 -0800

    Added setup_mac script and files

---
+++ setup_mac.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Configure the HOME/END buttons
+mkdir ~/Library/KeyBindings/
+cp DefaultKeyBinding.dict ~/Library/KeyBindings/
+
+# Copy iTerm2 config
+cp com.googlecode.iterm2.plist ~/Library/Preferences/
