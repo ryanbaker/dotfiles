d630a8e3d9938735f2fbc890c630385313356bfa
Ryan Baker <ryanb@cetoncorp.com>
2013-11-11 15:23:45 -0800

    Removed iTerm settings from setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -6,6 +6,3 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # Configure the HOME/END buttons
 mkdir -p ~/Library/KeyBindings/
 cp DefaultKeyBinding.dict ~/Library/KeyBindings/
-
-# Copy iTerm2 config
-cp com.googlecode.iterm2.plist ~/Library/Preferences/
