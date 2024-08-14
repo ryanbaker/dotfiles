bdef2b37389514bebda27a9dc13047e5f1f4c47d
Ryan Baker <ryan@compoundeye.com>
2024-08-14 15:26:31 -0700

    Updates to imwheel

--- imwheel.sh
+++ imwheel.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # script to fix scrolling in chrome. This needs to be added to autostart.
 # see here for more info:
 # https://askubuntu.com/questions/254367/permanently-fix-chrome-scroll-speed
